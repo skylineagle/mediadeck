a00f8c774d27948f86317c4903666bbf00b9e749
skylineagle <barmghr@gmail.com>
2025-01-01 23:02:20 +0200

    feat: activate metrics

---
+++ scripts/test-stream.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Stream test video pattern to RTSP server
+gst-launch-1.0 -vv videotestsrc ! videoconvert ! x264enc ! h264parse ! queue ! rtspclientsink latency=0 protocols=tcp location=rtsp://127.0.0.1:8554/mystream
